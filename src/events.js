d8073afb6225728c0f49ceba9c90e8bba81605a2
Paul Vollmer <paul.vollmer@protonmail.com>
2019-02-14 22:05:12 +0100

    created package.json and events.js

---
+++ src/events.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2019 myles.systems - All rights reserved.
+ */
+
+const EVENTS = {
+	PRODUCT_CREATED: 'product_created',
+	PRODUCT_UPDATED: 'product_updated',
+	PRODUCT_DELETED: 'product_deleted',
+	FAMILY_CREATED: 'family_created',
+	FAMILY_UPDATED: 'family_updated',
+	FAMILY_DELETED: 'family_deleted',
+	CATALOG_CREATED: 'catalog_created',
+	CATALOG_UPDATED: 'catalog_updated',
+	CATALOG_DELETED: 'catalog_deleted'
+};
+
+module.exports = EVENTS;
