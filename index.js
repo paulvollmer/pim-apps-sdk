71b3cfe745dfb91af5eb822bb7bee5222e0b8073
Paul Vollmer <paul.vollmer@protonmail.com>
2019-02-16 11:15:51 +0100

    created mockHeaders function

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 const EVENTS = require('./src/events');
 const signature = require('./src/signature');
 const middleware = require('./src/middleware');
+const testing = require('./src/testing');
 
 module.exports = {
   EVENTS,
   signature,
   middleware,
+  testing,
 };
