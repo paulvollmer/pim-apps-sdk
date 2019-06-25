e7a0435a4f1a5703804466b921f6c92985eeda6e
Paul Vollmer <paul.vollmer@protonmail.com>
2019-06-25 18:40:04 +0200

    Test index.js (#26)
    * testing index.js typeof
    
    * Bump version to 0.1.9
    
    * Updated readme version badge

--- index.test.js
+++ index.test.js
@@ -2,10 +2,30 @@
  * Copyright 2019 myles.systems - All rights reserved.
  */
 
-const { EVENTS } = require('./index');
+const {
+  EVENTS,
+  signature,
+  middleware,
+  testing,
+} = require('./index');
 
 describe('Test index', () => {
   test('EVENTS', () => {
-    expect(EVENTS.PRODUCT_CREATED).toBe('product_created');
+    expect(typeof EVENTS).toBe('object');
+  });
+
+  test('signature sign', () => {
+    expect(typeof signature.sign).toBe('function');
+  });
+  test('signature verify', () => {
+    expect(typeof signature.verify).toBe('function');
+  });
+
+  test('middleware', () => {
+    expect(typeof middleware).toBe('function');
+  });
+
+  test('testing mockHeaders', () => {
+    expect(typeof testing.mockHeaders).toBe('function');
   });
 });
